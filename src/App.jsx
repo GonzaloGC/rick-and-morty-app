c78e7f54c45b8e3e9154aa8389fef88974326475
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-06-01 21:01:53 -0400

    Api conection rick and morty with fetch and style for cards

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+// Supports weights 100-900
+import '@fontsource-variable/lexend-deca';
+import "./App.css";
+
+const App = () => {
+  const [img, setImg] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const dataRyM = async () => {
+      try {
+        const info = await fetch(
+          "https://rickandmortyapi.com/api/character/?page=19"
+        );
+        console.log(info)
+        if (!info.ok) {
+          throw new Error("TENEMOS UN ERROR GIGANTE");
+        }
+        const infojson = await info.json();
+        // console.log(infojson)
+        const jsonResult = infojson.results;
+        // const resultImages = jsonResult.image
+        console.log(jsonResult)
+        setImg(jsonResult);
+      } catch (error) {
+        console.log("este es un error:", error);
+        setError(error);
+      }
+    };
+    dataRyM();
+  }, []);
+
+  const imagesRyM = img.map((imgRyM) => (
+    <div key={imgRyM.id} className="container-img-rickAndMorty">
+      <img  src={imgRyM.image} alt="" />
+      <section className="container-info">
+        <div>
+            <span className="name">{imgRyM.name}
+            </span>
+          
+        </div>
+        <div>
+          <div>
+            <p className="type">Species:<br/>
+              <span className="species">{imgRyM.species}
+              </span>
+            </p>
+          </div>
+          <div>
+            <p>Gender:<br/>
+              <span className="gender">{imgRyM.gender}
+              </span>
+            </p>
+          </div>
+        </div>
+      </section>
+     </div>
+  ));
+
+  return (
+    <>
+      <section>
+        <img className="logo" src="public/Rick-and-Morty-logo.png" alt="" />
+        {imagesRyM}
+      </section>
+    </>
+  );
+};
+
+export default App;
