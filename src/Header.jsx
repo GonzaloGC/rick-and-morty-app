53b7502c81bceceef1cc8bd32b0a37c293c92e23
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-07-02 23:55:38 -0400

    added Header component

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <img className="logo" src="/Rick-and-Morty-logo.png" alt="" />
+      </header>
+    </>
+  );
+};
